7c12424d907fa11dce0db00d035e12396596eacb
foximaychik <128075298+foximaychik@users.noreply.github.com>
2024-03-07 09:45:40 +0400

    V1 adding

---
+++ Game.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class Obj {
+	public:
+		void ObjAdd(int num);
+		void ObjDel(int num);
+		int m_obj = 0;
+};
+void Obj::ObjAdd(int num) {
+	cout << "Objects added in number " << num << endl;
+	m_obj += num;
+}
+void Obj::ObjDel(int num) {
+	cout << "Objects deleted in number " << num << endl;
+	m_obj -= num;
+}
+
+
+int main() {
+	Obj obj1;
+
+	cout << "Now objects is " << obj1.m_obj << endl;
+	obj1.ObjAdd(10);
+	cout << "Now objects is " << obj1.m_obj << endl;
+	obj1.ObjDel(6);
+	cout << "Now objects is " << obj1.m_obj << endl;
+
+	return 0;
+}
